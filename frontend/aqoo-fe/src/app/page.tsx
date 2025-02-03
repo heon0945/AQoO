ebaf34b01780df5b99d12b1978cfeae5c3ac88bd
ejoyee <whdmsdl401@gmail.com>
2025-02-03 16:31:35 +0900

    test : 젠킨스 테스트용

--- frontend/aqoo-fe/src/app/page.tsx
+++ frontend/aqoo-fe/src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600">Hello, Next.js 14!</h1>
       <p className="mt-4 text-lg text-gray-700">Tailwind CSS 적용 완료 ✅</p>
+      <p className="mt-4 text-lg text-gray-700">젠킨스 테스트!</p>
     </main>
   );
 }
