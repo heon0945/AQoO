416a93aaae7bac05ecfd0fa55ef596927ffb5e6b
heon <songhun829@gmail.com>
2025-02-06 15:15:35 +0900

    package json file

--- frontend/aqoo-fe/next.config.mjs
+++ frontend/aqoo-fe/next.config.mjs
@@ -2,9 +2,9 @@
 const nextConfig = {
   output: 'export',
   reactStrictMode: true,
-  experimental: {
-    appDir: true, // Next.js 14에서 App Router 사용 가능
-  },
+  //experimental: {
+  //  appDir: true, // Next.js 14에서 App Router 사용 가능
+  //},
 };
 
 export default nextConfig;
