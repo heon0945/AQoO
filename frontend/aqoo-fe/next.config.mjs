20e2aef426b6b9d643dc8cca59f8ff29a271602e
heon <songhun829@gmail.com>
2025-02-06 17:22:32 +0900

    jenkins test

--- frontend/aqoo-fe/next.config.mjs
+++ frontend/aqoo-fe/next.config.mjs
@@ -2,12 +2,11 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     appDir: true,  // App Router 사용 (필요한 경우)
-  }
+  },
   exportTrailingSlash: true,  // URL 끝에 슬래시 추가
   images: {
     unoptimized: true,  // 이미지 최적화 비활성화
   },
-  
 };
 
 export default nextConfig;
