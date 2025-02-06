9526230b2b668b74b4fa93dcf283887282042c79
heon <songhun829@gmail.com>
2025-02-06 16:15:43 +0900

    edit files for nginx

--- frontend/aqoo-fe/next.config.mjs
+++ frontend/aqoo-fe/next.config.mjs
@@ -1,10 +1,13 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   reactStrictMode: true,
-  //experimental: {
-  //  appDir: true, // Next.js 14에서 App Router 사용 가능
-  //},
+  experimental: {
+    appDir: true,  // App Router 사용 (필요한 경우)
+  },
+  output: 'export',  // 정적 사이트 export 설정
+  exportTrailingSlash: true,  // URL 끝에 슬래시 추가
+  images: {
+    unoptimized: true,  // 이미지 최적화 비활성화
+  },
 };
 
 export default nextConfig;
