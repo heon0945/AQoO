e75bfcbea7c3caa32a531e5435ed41a25f0062cd
heon <songhun829@gmail.com>
2025-02-06 17:15:56 +0900

    jenkins ha

--- frontend/aqoo-fe/next.config.mjs
+++ frontend/aqoo-fe/next.config.mjs
@@ -2,8 +2,7 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     appDir: true,  // App Router 사용 (필요한 경우)
-  },
-  output: 'export',  // 정적 사이트 export 설정
+  }
   exportTrailingSlash: true,  // URL 끝에 슬래시 추가
   images: {
     unoptimized: true,  // 이미지 최적화 비활성화
