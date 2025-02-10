80d1a8c50315a3ee77109fa6f0d0862ab54ff85c
이진호 <jjin4363@gmail.com>
2025-02-10 12:32:20 +0900

    feat: 회원탈퇴 API 개발 완료

--- backend/aqoo/src/main/java/org/com/aqoo/domain/auth/controller/UserController.java
+++ backend/aqoo/src/main/java/org/com/aqoo/domain/auth/controller/UserController.java
@@ -1,9 +1,11 @@
 package org.com.aqoo.domain.auth.controller;
 
+import com.nimbusds.oauth2.sdk.http.HTTPRequest;
 import lombok.RequiredArgsConstructor;
 import org.com.aqoo.domain.auth.dto.*;
 import org.com.aqoo.domain.auth.service.AuthService;
 import org.com.aqoo.domain.auth.service.UserService;
+import org.com.aqoo.util.JwtUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService userService;
+    private final JwtUtil jwtUtil;
 
     // 회원정보 조회
     @GetMapping("/{userId}")
@@ -43,6 +46,25 @@ public class UserController {
     @GetMapping("/isJoined/{email}")
     public ResponseEntity<Boolean> isArleadyAccount(@PathVariable String email) {
         return ResponseEntity.ok(userService.isAlreadyJoin(email));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> withdraw (@RequestBody WithRawRequest request, @RequestHeader("Authorization") String authHeader){
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String jwt = authHeader.substring(7);
+            try {
+                // JWT 토큰에서 userId (또는 username) 추출
+                String userId = jwtUtil.extractUsername(jwt);
+                if(userId.equals(request.getUserId())){
+                    userService.changeStatus(request.getUserId());
+                }
+                return ResponseEntity.ok("회원상태 변경 완료" + userId);
+            } catch (Exception e) {
+                return ResponseEntity.badRequest().body("JWT 파싱 오류: " + e.getMessage());
+            }
+        } else {
+            return ResponseEntity.badRequest().body("Authorization 헤더가 없거나 유효하지 않습니다.");
+        }
     }
 
 }
