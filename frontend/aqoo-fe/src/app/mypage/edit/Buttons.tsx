b7051ee6c5fa66f95981cddcfc014eb4ec0e5c61
JonghwaLee <misterlee95@gmail.com>
2025-02-11 09:57:56 +0900

    before merge

--- frontend/aqoo-fe/src/app/mypage/edit/Buttons.tsx
+++ frontend/aqoo-fe/src/app/mypage/edit/Buttons.tsx
@@ -29,22 +29,3 @@ export default function Buttons({ text }: ButtonsProps) {
     </button>
   );
 }
-
-// "use client";
-
-// import Link from "next/link";
-
-// export default function LeftButtons() {
-//   return (
-//     <div className="flex flex-col justify-between m-2">
-//       <Link
-//         href="/mypage"
-//         className=" min-w-[80px] h-10 px-2 rounded-xl border border-[#040303] bg-white
-//         [box-shadow:-2px_-2px_0px_1px_rgba(0,0,0,0.5)_inset] flex items-center justify-center text-[#070707] text-center
-//         font-[400] text-2xl leading-none font-[NeoDunggeunmo_Pro] "
-//       >
-//         Back
-//       </Link>
-//     </div>
-//   );
-// }
