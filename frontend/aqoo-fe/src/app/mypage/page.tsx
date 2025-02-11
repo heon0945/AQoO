b502d2954df437b9d818ce844e159b2de4e40360
JonghwaLee <misterlee95@gmail.com>
2025-02-11 15:14:19 +0900

    mypage button component 삭제 및 통합 / logout 기능 구현

--- frontend/aqoo-fe/src/app/mypage/page.tsx
+++ frontend/aqoo-fe/src/app/mypage/page.tsx
@@ -1,17 +1,32 @@
 "use client";
 
-import LeftButtons from "./components/LeftButtons";
 import MyCollection from "./components/MyCollection";
 import Profile from "./components/Profile";
 
 import { useUserFishCollectionTest } from "@/hooks/useUserFishCollection";
 import { useAllFishCollectionTest } from "@/hooks/useAllFishCollection";
 import { useCustomFishCollectionTest } from "@/hooks/useCustomFishCollection";
+import Link from "next/link";
+import { useAuth } from "@/hooks/useAuth";
+import { useRouter } from "next/navigation";
 
 export default function MyPage() {
+  const { logout } = useAuth();
+  const router = useRouter();
+
   const { fishList: userFishList } = useUserFishCollectionTest();
   const { fishList: allFishList } = useAllFishCollectionTest();
   const { fishList: customFishList } = useCustomFishCollectionTest();
+
+  // 로그아웃 기능 핸들
+  const handleLogout = async () => {
+    try {
+      await logout();
+      router.push("/");
+    } catch (error) {
+      console.error("Logout Failed:", error);
+    }
+  };
 
   return (
     <div
@@ -20,10 +35,33 @@ export default function MyPage() {
         h-screen 
         bg-[url('/images/배경샘플.png')] 
         bg-cover bg-center bg-no-repeat
+        relative
       "
     >
       {/* 왼쪽 상단 버튼 */}
-      <LeftButtons />
+      <Link
+        href="/main"
+        className="absolute top-2 left-2 min-w-[80px] h-10 px-2 rounded-xl border border-[#040303] bg-white
+        [box-shadow:-2px_-2px_0px_1px_rgba(0,0,0,0.5)_inset] flex items-center justify-center text-[#070707] text-center
+        font-[400] text-2xl leading-none font-[NeoDunggeunmo_Pro] "
+      >
+        Home
+      </Link>
+      <button
+        onClick={handleLogout}
+        className="
+        absolute bottom-2 left-2
+          min-w-[80px] h-10 px-2 mt-2
+          rounded-xl border border-[#040303] bg-white 
+          [box-shadow:-2px_-2px_0px_1px_rgba(0,0,0,0.5)_inset]
+          flex items-center justify-center
+          text-[#070707] text-center font-[400] text-2xl leading-none
+          font-[NeoDunggeunmo_Pro]
+        "
+      >
+        Logout
+      </button>
+
       {/* 메인 컨테이너 (내 정보 도감) */}
       <div
         className="flex flex-col items-center flex-1
