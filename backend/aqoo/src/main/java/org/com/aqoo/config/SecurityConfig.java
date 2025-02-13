c382fe2b1412e7647e4ed6b98392ccf4db51322a
이진호 <jjin4363@gmail.com>
2025-02-13 17:51:38 +0900

    시큐리티 수저ㅏㅇ

--- backend/aqoo/src/main/java/org/com/aqoo/config/SecurityConfig.java
+++ backend/aqoo/src/main/java/org/com/aqoo/config/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/users/isActivate/*",
                                 "/api/v1/auth/register/*",
                                 "/api/v1/auth/login/*",
+                                "/api/v1/users/*",
                                 "/oauth2/*",
                                 "/api/v1/auth/validate-id",
                                 "/api/v1/auth/validate-email",
