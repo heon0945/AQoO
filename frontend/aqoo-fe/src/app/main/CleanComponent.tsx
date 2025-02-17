567b7cdadb3f80ab213031f3b07606c4765dbdc8
ejoyee <whdmsdl401@gmail.com>
2025-02-17 12:09:39 +0900

    fix : 메인 페이지 사운드 및 어항 상태별 효과 추가 (+ 컴포넌트별 소리 및 오류 수정)

--- frontend/aqoo-fe/src/app/main/CleanComponent.tsx
+++ frontend/aqoo-fe/src/app/main/CleanComponent.tsx
@@ -200,6 +200,7 @@ export default function CleanComponent({
       if (videoRef.current && videoRef.current.srcObject) {
         const stream = videoRef.current.srcObject as MediaStream;
         stream.getTracks().forEach((track) => track.stop());
+        videoRef.current.srcObject = null; // ✅ 비디오 스트림 해제
       }
     };
   }, [isMirrored, palmImage]);
