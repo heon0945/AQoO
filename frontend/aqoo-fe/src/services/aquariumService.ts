c0cdda0ba323070a196d229af2a984ec6ad26361
이진호 <jjin4363@gmail.com>
2025-02-18 14:59:28 +0900

    test

--- frontend/aqoo-fe/src/services/aquariumService.ts
+++ frontend/aqoo-fe/src/services/aquariumService.ts
@@ -6,6 +6,6 @@ import axiosInstance from "./axiosInstance";
 const API_BASE_URL = "https://i12e203.p.ssafy.io/api/v1";
 
 export const getUserFishes = async (aquariumId: number) => {
-  const response = await axiosInstance.get(`/aquarium/fish/${aquariumId}`);
+  const response = await axiosInstance.get(`/aquariums/fish/${aquariumId}`);
   return response.data;
 };
