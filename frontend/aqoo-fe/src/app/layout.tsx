ace31ce36809c7777209bc98b4fd4cb04f8aeb09
최영환 <dlsxj101@naver.com>
2025-02-19 15:05:31 +0900

    google analytics 코드 추가

--- frontend/aqoo-fe/src/app/layout.tsx
+++ frontend/aqoo-fe/src/app/layout.tsx
@@ -1,6 +1,7 @@
 // app/layout.tsx
 "use client"; // 클라이언트 컴포넌트 설정
 
+import Script from "next/script";
 import "@/styles/globals.css"; // Tailwind 등 글로벌 스타일 적용
 import BackgroundMusic from "@/components/BackgroundMusic";
 import Navbar from "@/components/NavBar";
@@ -28,8 +29,28 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <meta name="theme-color" content="#000000" />
         {/* iOS 홈 화면 아이콘 */}
         <link rel="apple-touch-icon" sizes="180x180" href="/icon/icon-180.png" />
+
+        {/* Google Tag Manager 스크립트 */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PK6QF66G');
+          `}
+        </Script>
       </head>
       <body className="w-full min-h-screen sm:overflow-auto">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PK6QF66G"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <div className="relative w-full min-h-screen overflow-x-hidden">
           <RecoilProvider>
             <BackgroundMusic /> {/* 배경 음악 실행 */}
