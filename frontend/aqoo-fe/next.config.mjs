03ab3b1b472a3575282ff1cfc589de9c0b598a3a
최영환 <dlsxj101@naver.com>
2025-02-19 09:09:24 +0900

    잰킨스 빌드 오류 해결

--- frontend/aqoo-fe/next.config.mjs
+++ frontend/aqoo-fe/next.config.mjs
@@ -1,4 +1,4 @@
-import withPWA from 'next-pwa';
+import withPWA from "next-pwa";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -7,12 +7,11 @@ const nextConfig = {
     appDir: true, // Next.js 14에서 App Router 사용 가능
   },
   images: {
-    domains: ['i12e203.p.ssafy.io'], // ✅ 외부 이미지 도메인 추가
+    domains: ["i12e203.p.ssafy.io"],
   },
 };
 
 export default withPWA({
-  dest: 'public', // 빌드시 생성될 Service Worker 관련 파일들이 public 폴더에 저장됩니다.
-  disable: process.env.NODE_ENV === 'development', // 개발 환경에서는 PWA 비활성화
-  ...nextConfig,
-});
+  dest: "public", // SW 파일 등 PWA 관련 파일이 저장될 경로
+  disable: process.env.NODE_ENV === "development", // 개발 환경에서는 PWA 비활성화
+})(nextConfig);
