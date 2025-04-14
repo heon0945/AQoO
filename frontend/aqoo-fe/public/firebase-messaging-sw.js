f26fa5e46b71f02b2ced7a97033d929ab6d700c7
이진호 <jjin4363@gmail.com>
2025-04-14 12:11:54 +0900

    Update frontend/aqoo-fe/public/firebase-messaging-sw.js, frontend/aqoo-fe/src/lib/firebase.ts, frontend/aqoo-fe/src/components/NotificationComponent.tsx

--- frontend/aqoo-fe/public/firebase-messaging-sw.js
+++ frontend/aqoo-fe/public/firebase-messaging-sw.js
@@ -4,13 +4,13 @@ importScripts("https://www.gstatic.com/firebasejs/11.2.0/firebase-app-compat.js"
 importScripts("https://www.gstatic.com/firebasejs/11.2.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyD331gS46jCW24EjPrIk6ZLM1zOF-7FAJU",
-  authDomain: "aqoo-6ff04.firebaseapp.com",
-  projectId: "aqoo-6ff04",
-  storageBucket: "aqoo-6ff04.firebasestorage.app",
-  messagingSenderId: "477360750442",
-  appId: "1:477360750442:web:171a77b5cb7528aea26f13",
-  measurementId: "G-9WT1MZZREY",
+  apiKey: FIREBASE-API-KEY,
+  authDomain: AUTH-DOMAIN,
+  projectId: PROJECT-ID,
+  storageBucket: STORAGEBUCKET,
+  messagingSenderId: FIREBASE-MESSAGE,
+  appId: FIREBASE-APP-ID,
+  measurementId: MEASUREMENT-ID,
 });
 
 const messaging = firebase.messaging();
